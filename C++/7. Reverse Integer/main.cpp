4a91f79210691ab4389a3255733dbc148530b974
fengzhu <fengzhu@blizzmi.com>
2017-08-04 19:00:32 +0800

    test 7

--- C++/7. Reverse Integer/main.cpp
+++ C++/7. Reverse Integer/main.cpp
@@ -6,6 +6,8 @@ int main()
 	solution s;
 	std::cout << s.reverse(123) << std::endl;
 	std::cout << s.reverse(-123) << std::endl;
+	std::cout << s.reverse(125384977) << std::endl;
+	std::cout << s.reverse(-125384977) << std::endl;
 
 	return 0;
 }
