216f2667bcfda6e188f04753b80d89dcb0407d4e
fengzhu <fengzhu@blizzmi.com>
2018-03-16 13:15:11 +0800

    module中文化

--- python/djcode/Mxonline3/apps/courses/models.py
+++ python/djcode/Mxonline3/apps/courses/models.py
@@ -15,7 +15,7 @@ class Course(models.Model):
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     # TextField允许我们不输入长度。可以输入到无限大。暂时定义为TextFiled，之后更新为富文本
     detail = models.TextField(verbose_name=u"课程详情")
-    degree = models.CharField(choices=DEGREE_CHOICES, max_length=2)
+    degree = models.CharField(choices=DEGREE_CHOICES, max_length=2, verbose_name=u"学习难度")
     # 使用分钟做后台记录(存储最小单位)前台转换
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
     # 保存学习人数:点击开始学习才算
@@ -33,6 +33,9 @@ class Course(models.Model):
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 # 章节
 class Lesson(models.Model):
@@ -46,6 +49,9 @@ class Lesson(models.Model):
         verbose_name = u"章节"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return '《{0}》课程的章节 >>{1}'.format(self.course, self.name)
+
 
 # 每章视频
 class Video(models.Model):
@@ -58,6 +64,9 @@ class Video(models.Model):
     class Meta:
         verbose_name = u"视频"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}章节的视频 >>{1}'.format(self.lesson, self.name)
 
 
 # 课程资源
@@ -77,3 +86,6 @@ class CourseResource(models.Model):
     class Meta:
         verbose_name = u"课程资源"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}课程的资源 >>{1}'.format(self.course, self.name)
--- python/djcode/Mxonline3/apps/organization/models.py
+++ python/djcode/Mxonline3/apps/organization/models.py
@@ -15,6 +15,9 @@ class CityDict(models.Model):
         verbose_name = u"城市"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 # 课程机构
 class CourseOrg(models.Model):
@@ -37,6 +40,9 @@ class CourseOrg(models.Model):
         verbose_name = u"课程机构"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return '课程机构{0}'.format(self.name)
+
 
 # 讲师
 class Teacher(models.Model):
@@ -55,3 +61,6 @@ class Teacher(models.Model):
     class Meta:
         verbose_name = u"教师"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "[{0}]的教师: {1}".format(self.org, self.name)
