d15a6a7d332ecce2b8238b1bd39c477bbe83f945
fengzhu <fengzhu@blizzmi.com>
2018-03-16 10:05:48 +0800

    add xadmin

---
+++ python/djcode/Mxonline3/apps/users/xadmin.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+__author__ = 'fengzhu'
+__date__ = '18-3-16 上午9:44'
+
+import xadmin
+
+from .models import EmailVerifyRecord
+
+
+class EmailVerifyRecordAdmin(object):
+    pass
+
+
+
+
+
