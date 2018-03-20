9213aace12761d0de3c0bdbc1aa91ef9093484a4
fengzhu <fengzhu@blizzmi.com>
2018-03-20 14:44:22 +0800

    机构列表展示页

--- python/djcode/Mxonline3/Mxonline3/urls.py
+++ python/djcode/Mxonline3/Mxonline3/urls.py
@@ -19,6 +19,7 @@ import xadmin
 from django.views.generic import TemplateView
 # from users.views import user_login
 from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
+from organization.views import OrgView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -41,4 +42,7 @@ urlpatterns = [
 
     # 修改密码url; 用于passwordreset页面提交表单
     path('modify_pwd/', ModifyPwdView.as_view(), name="modify_pwd"),
+
+    # 课程机构首页url
+    path('org_list/', OrgView.as_view(), name="org_list"),
 ]
--- python/djcode/Mxonline3/apps/organization/views.py
+++ python/djcode/Mxonline3/apps/organization/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
+# 处理课程机构列表的view
+class OrgView(View):
+    def get(self, request):
+        return render(request, "org_list.html", {})
+
